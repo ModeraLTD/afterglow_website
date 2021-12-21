698ea85602cb06576d985eb3bf79cf477557f678
Cyclip <gdneop@gmail.com>
2021-12-21 19:01:54 +0000

    Create Product model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    """Model representing a single product"""
+
+    name = models.CharField(max_length=16)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField()
+
+    def __str__(self):
+        return self.name
